9fe6ed95f73054cb010a14071bc57197522275b4
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2014-08-13 17:25:33 +0200

    v0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="python-svgsynoptic",
-    version="0.1.0",
+    version="0.2.0",
     description="Widget for displaying a SVG synoptic.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
